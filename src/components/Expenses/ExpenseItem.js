9b3188ce46d2c9c5542afc39467af374b33267a4
LefebvreAnthony <anthony.lfb@hotmail.fr>
2022-11-13 15:18:54 +0100

    add new component and css form

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ title, amount, date }) => {
+  const [Title, setTitle] = useState(title);
+  const clickHandler = () => {
+    setTitle("Updated!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{Title}</h2>
         <div className="expense-item__price">{"$ " + amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 };
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,26 +5,14 @@ import "./Expenses.css";
 const Expenses = ({ expenses }) => {
   return (
     <Card className="expenses">
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amout}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amout}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amout}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amout}
-        date={expenses[3].date}
-      />
+      {expenses.map((expense, index) => (
+        <ExpenseItem
+          key={index}
+          title={expense.title}
+          amount={expense.amout}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
 };
