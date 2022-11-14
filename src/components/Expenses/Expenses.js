091fc8c690acc08505fe3e5188928a17027da0b0
LefebvreAnthony <anthony.lfb@hotmail.fr>
2022-11-14 10:47:36 +0100

    add new component and css for  filter by year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,19 +1,35 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = ({ expenses }) => {
+  const [filteredYear, setfilteredYear] = useState("2020");
+
+  const FilterHandler = (selectedYear) => {
+    setfilteredYear(selectedYear);
+    console.log("This is in Expense.js");
+    console.log(selectedYear);
+  };
+
   return (
-    <Card className="expenses">
-      {expenses.map((expense, index) => (
-        <ExpenseItem
-          key={index}
-          title={expense.title}
-          amount={expense.amout}
-          date={expense.date}
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selectedYear={filteredYear}
+          onFilterChange={FilterHandler}
         />
-      ))}
-    </Card>
+        {expenses.map((expense, index) => (
+          <ExpenseItem
+            key={index}
+            title={expense.title}
+            amount={expense.amout}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
   );
 };
 
